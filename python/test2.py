b2d0075ad441bf61949b74e28273f8f26139ee8a
lukas <lukas.petersson.1999@gmail.com>
2023-03-02 22:18:13 +0100

    test 2

--- python/test2.py
+++ python/test2.py
@@ -1,4 +1,5 @@
 """Script to run langchain-server locally using docker-compose."""
+import numpy as np
 import subprocess
 from pathlib import Path
 
@@ -6,8 +7,8 @@ from pathlib import Path
 def main() -> None:
     """Run the langchain server locally."""
     p = Path(__file__).absolute().parent / "docker-compose.yaml"
-    subprocess.run(["docker-compose", "-f", str(p), "pull"])
-    subprocess.run(["docker-compose", "-f", str(p), "up"])
+    subprocess.call(["docker-compose", "-f", str(p), "pull"])
+    subprocess.call(["docker-compose", "-f", str(p), "up"])
 
 
 if __name__ == "__main__":
